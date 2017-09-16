76841dbdadb7986e0173cb13dd193ba3bc943e6e
Dhiva04 <dhivakaran.ret@gmail.com>
2017-09-17 04:53:32 +0530

    final

--- routes/register.js
+++ routes/register.js
@@ -91,7 +91,7 @@ const watcher = chokidar.watch('./in').on('add', (path) => {
 		    	   console.log(data);
 		    	   if(data.status=="pending"){
 		    		   var params = {
-		    					image:'https://murmuring-plains-24395.herokuapp.com/in/'+split,
+		    					image:'https://agile-citadel-92310.herokuapp.com/in/'+split,
 		    				    gallery_name:'RET'	
 		    			};
 		    		     		 
@@ -164,7 +164,7 @@ const watcher1 = chokidar.watch('./out').on('add', (path) => {
 	if(user){
 		if(user.status=="pending"){
 			var params = {
-					image:'https://murmuring-plains-24395.herokuapp.com/out/'+split,
+					image:'https://agile-citadel-92310.herokuapp.com/out/'+split,
 				    gallery_name:'RET'	
 			};
 		     			 
@@ -224,7 +224,7 @@ const watcher1 = chokidar.watch('./out').on('add', (path) => {
 		    	   console.log(data);
 		    	   if(data.status=="pending"){
 		    		   var params = {
-		    					image:'https://murmuring-plains-24395.herokuapp.com/out/'+split,
+		    					image:'https://agile-citadel-92310.herokuapp.com/out/'+split,
 		    				    gallery_name:'RET'	
 		    			};
 		0    		     			 
